4c58e1b76be8eed76f2bb88be96f9c5904c23ea2
Apoorv Sachan <apoorv.99.sachan@gmail.com>
2020-02-13 21:01:54 +0530

    shitty syntax analysis done

--- src/com/artevaluator/Main.java
+++ src/com/artevaluator/Main.java
@@ -1,19 +1,23 @@
 package com.artevaluator;
 import com.artevaluator.grammar.Lexer;
 import  com.artevaluator.grammar.Token;
+import com.artevaluator.grammar.SemanticAnalyser;
 
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Token> a = new ArrayList<Token>();
         Lexer lexr = new Lexer(args[0]+'\0');
         lexr.tokenize();
-        a = lexr.getTokenized();
+        ArrayList<Token> a = lexr.getTokenized();
 
         for (int i = 0; i < a.size(); i ++){
             System.out.println(a.get(i).tokToString());
         }
+
+        SemanticAnalyser ss = new SemanticAnalyser(args[0]+'\0');
+        ss.parse();
+
     }
 }
--- src/com/artevaluator/grammar/SemanticAnalyser.java
+++ src/com/artevaluator/grammar/SemanticAnalyser.java
@@ -2,4 +2,105 @@ package com.artevaluator.grammar;
 
 public class SemanticAnalyser {
 
+    Terminals tr;
+    String expression;
+    int index;
+    Character lookahead;
+
+    void match(Character t){
+        if (lookahead == t) {
+            index++;
+            lookahead=expression.charAt(index);
+        }
+        else {
+            System.out.println("Error");
+            System.out.println("Expected : " + t);
+        }
+    }
+    void A(){
+        System.out.print(lookahead);
+        if (tr.getType(lookahead) == "digit") {
+            match(lookahead);
+            B();
+        }
+        else if(tr.getType(lookahead) == "basicOperator") {
+            match(lookahead);
+            C();
+        }
+        else {
+            return;
+        }
+    }
+
+    void B(){
+        System.out.print(lookahead);
+        if(tr.getType(lookahead) == "basicOperator"){
+            match(lookahead);
+            C();
+        }
+        else if(lookahead == '.') {
+            match('.');
+            D();
+        }
+        else if(tr.getType(lookahead) == "digit"){
+            match(lookahead);
+            B();
+        }
+        else{
+           return;
+        }
+    }
+    void C(){
+        System.out.print(lookahead);
+        if(tr.getType(lookahead) == "digit") {
+            match(lookahead);
+            B();
+        }
+        else if(lookahead == '+') {
+            match('+');
+            C();
+        }
+        else if(lookahead == '-') {
+            match('-');
+            C();
+        }
+        else if(lookahead == '*') {
+            match('*');
+            C();
+        }
+        else{
+            return;
+        }
+    }
+
+    void D(){
+        System.out.print(lookahead);
+        if (tr.getType(lookahead) == "basicOperator") {
+            match(lookahead);
+            C();
+        }
+        else if(tr.getType(lookahead) == "digit") {
+            match(lookahead);
+            D();
+        }
+        else{
+            return;
+        }
+    }
+
+    public void parse(){
+        A();
+        if (lookahead == '\0') {
+            System.out.println("parse Success!!");
+        } else {
+            System.out.println("Syntax Error !!");
+        }
+    }
+    public SemanticAnalyser(String a) {
+        expression = a;
+        index = 0;
+        lookahead = expression.charAt(index);
+        tr = new Terminals();
+    }
+
 }
