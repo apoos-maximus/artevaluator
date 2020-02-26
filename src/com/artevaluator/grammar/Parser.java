63e5254babe10e42e964580e078ac9edbffd1c50
Apoorv Sachan <apoorv.99.sachan@gmail.com>
2020-02-26 16:31:00 +0530

    added factor()

--- src/com/artevaluator/grammar/Parser.java
+++ src/com/artevaluator/grammar/Parser.java
@@ -12,9 +12,36 @@ public class Parser {
         return tokenStream.get(tokPointer);
     }
 
-
-
-
+    NodePack factor(){
+        NodePack result = new NodePack();
+        NodePack a = new NodePack();
+        NodePack b = new NodePack();
+        NodePack c = new NodePack();
+        result.result = true;
+        if ( curTok.tokCheckVal("(") ){
+            curTok = nextToken();
+            a = expr();
+            if(a.result == false){
+                result.result = false;
+            }
+            else if (curTok.tokCheckVal(")")){
+                System.out.println("Syntsx Error[expr()] -- missing ')' ");
+                result.result = false;
+            }
+            else{
+                curTok = nextToken();
+            }
+        }
+        else if (curTok.tokCheckType("operand")){
+            result.aNode = new Node(curTok);
+            curTok = nextToken();
+        }
+        else{
+            System.out.println("Syntax Error.. !");
+            result.result = false;
+        }
+    return result;
+    }
     public Parser(ArrayList<Token> a) {
         tokenStream = a;
         tokPointer = -1;
